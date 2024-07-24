9b4c2a7ed795f2a201a102a98223841119a13929
MembaMcetywa <iamemba10@gmail.com>
2024-07-24 11:40:04 +0200

    added places controller

---
+++ apps/backend/src/places/places.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get, Query } from '@nestjs/common';
+import { PlacesService } from './places.service';
+
+@Controller('api')
+export class PlacesController {
+  constructor(private placesService: PlacesService) {}
+
+  @Get('/places')
+  getPlaces(
+    @Query('location') location: string,
+    @Query('radius') radius: number,
+  ) {
+    return this.placesService.findPlaces(location, radius);
+  }
+}
