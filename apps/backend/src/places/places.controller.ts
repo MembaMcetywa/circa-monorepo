34af08297ef06667173c3a158a25ab79151b1a9f
MembaMcetywa <iamemba10@gmail.com>
2024-07-31 12:21:25 +0200

    added suggestions endpoint

--- apps/backend/src/places/places.controller.ts
+++ apps/backend/src/places/places.controller.ts
@@ -1,15 +1,27 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { PlacesService } from './places.service';
+import { LatLng } from '@googlemaps/google-maps-services-js';
 
 @Controller('api')
 export class PlacesController {
   constructor(private placesService: PlacesService) {}
 
   @Get('/places')
-  getPlaces(
+  async getPlaces(
     @Query('location') location: string,
     @Query('radius') radius: number,
   ) {
     return this.placesService.findPlacesNearby(location, radius);
+  }
+
+  @Get('suggestions')
+  async getPlaceSuggestions(
+    @Query('input') input: string,
+    @Query('lat') lat: number,
+    @Query('lng') lng: number,
+    @Query('radius') radius: number,
+  ) {
+    const location: LatLng = { lat, lng };
+    return this.placesService.getPlaceSuggestions(input, location, radius);
   }
 }
