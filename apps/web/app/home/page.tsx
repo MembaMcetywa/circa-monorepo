e14d00dc29c499932d96341e195307b7a01330a4
MembaMcetywa <iamemba10@gmail.com>
2024-08-02 14:04:17 +0200

    added home screen with map

--- apps/web/app/home/page.tsx
+++ apps/web/app/home/page.tsx
@@ -1,10 +1,14 @@
+"use client"
 import styles from "../page.module.css";
+import { GoogleMapsEmbed } from "@next/third-parties/google";
+import Map from "../components/Map";
 
 export default function Home() {
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <h1>Dashboard 1one</h1>
+    <div>
+      <main>
+        <Map />
       </main>
     </div>
   );
