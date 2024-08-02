cc259b703a29ac6d1a4fb9009b31ab9f871f7527
MembaMcetywa <iamemba10@gmail.com>
2024-08-02 11:24:41 +0200

    added navigation action to landing page start journey button

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 
+import Link from "next/link";
 import Button from "./components/Button";
 import styles from "./page.module.css";
 
 export default function Home() {
   const handleClick = () => {
-    alert("Button clicked!");
+    console.log('button action started')
   };
 
   return (
@@ -13,9 +14,12 @@ export default function Home() {
       <main className={styles.main}>
         <header>CIRCA</header>
         <p>
-          A <span style={{ color: "#EA3D2D" }}>neat</span> collection of interesting places
+          A <span style={{ color: "#EA3D2D" }}>neat</span> collection of
+          interesting places
         </p>
-        <Button onClick={handleClick}>Start journey</Button>
+        <Link href={"/home"}>
+          <Button onClick={handleClick}>Start journey</Button>
+        </Link>
       </main>
     </div>
   );
