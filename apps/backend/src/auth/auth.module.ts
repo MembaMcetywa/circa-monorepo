39281b0ca7c943618839055a4ef64f82a22c3900
MembaMcetywa <iamemba10@gmail.com>
2024-08-06 15:09:53 +0200

    added support for auth module

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
+import { User, UserSchema } from '../schemas/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const JWT_SECRET = process.env.JWT_SECRET;
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    PassportModule,
+    JwtModule.register({
+      secret: JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [AuthService, JwtStrategy],
+  controllers: [AuthController],
 })
 export class AuthModule {}
