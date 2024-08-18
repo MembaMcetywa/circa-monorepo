a5a7018a9144248fb206576c19d6f4a8bc4e412c
MembaMcetywa <iamemba10@gmail.com>
2024-08-18 12:26:47 +0200

    added base registration function. works.

--- apps/web/app/register/page.tsx
+++ apps/web/app/register/page.tsx
@@ -4,6 +4,7 @@ import TextInput from "../components/TextInput";
 import useAuthStore from "../store/authStore";
 import styles from "../auth.module.css";
 import Button from "../components/Button";
+import axios from "axios";
 
 
 const Register: React.FC = () => {
@@ -12,15 +13,35 @@ const Register: React.FC = () => {
   const [error, setError] = useState("");
   const login = useAuthStore((state) => state.login);
 
-  const handleRegister = async (e: React.FormEvent) => {
-    e.preventDefault();
+const handleRegister = async (e: React.FormEvent) => {
+  e.preventDefault();
 
-  };
+  try {
+    const response = await axios.post(
+      "http://localhost:3001/api/register",
+      { email, password },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log(response.status)
+    if (response.status !== 201) {
+      throw new Error("Registration failed");
+    }
+
+    const data = response.data;
+    login(data.accessToken, data.refreshToken);
+  } catch (err: any) {
+    setError(err.message);
+  }
+};
 
   return (
     <div className={styles.container}>
       <h2>Register</h2>
-      <form onSubmit={handleRegister}>
+      <form>
         <TextInput
           type="email"
           placeholder="Enter your email"
@@ -38,7 +59,7 @@ const Register: React.FC = () => {
           required
         />
         {error && <p className={styles["error-text"]}>{error}</p>}
-        <Button onClick={() => console.log('registration')}>Register</Button>
+        <Button className={styles.button} onClick={handleRegister}>Register</Button>
       </form>
     </div>
   );
