d9a0eadc2276e80975bc966b41c1a215e3340b4e
MembaMcetywa <iamemba10@gmail.com>
2024-09-01 19:24:49 +0200

    check if authed before navigating to home screen

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,22 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Button from "./components/Button";
 import styles from "./page.module.css";
+import useAuthStore from "./stores/authStore"
 
 export default function Home() {
+  const router = useRouter();
+  const { isAuthenticated } = useAuthStore();
+
   const handleClick = () => {
-    console.log('button action started')
+    if (isAuthenticated) {
+      router.push("/home");
+    } else {
+      console.error("User is not authenticated");
+      router.push("/login");
+    }
   };
 
   return (
@@ -17,9 +27,8 @@ export default function Home() {
           A <span style={{ color: "#EA3D2D" }}>neat</span> collection of
           interesting places
         </p>
-        <Link href={"/home"}>
-          <Button onClick={handleClick}>Start journey</Button>
-        </Link>
+        <Button onClick={handleClick}>Start journey</Button>{" "}
+
       </main>
     </div>
   );
